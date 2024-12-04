769069878374e91addddc345e934628c74e2d82e
AliZiabary11 <aliziabary@gmail.com>
2024-12-04 20:43:11 +0330

    refactor starship list ui

--- src/app/pages/starships-list/starships-list.component.ts
+++ src/app/pages/starships-list/starships-list.component.ts
@@ -1,31 +1,86 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
 import { selectAllStarships, selectLoading, selectSelectedStarship } from '../../store/selectors/starship.selectors';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/state/app.state';
 import { loadStarshipDetails, loadStarships } from '../../store/actions/starship.actions';
 import { AsyncPipe } from '@angular/common';
+import { Subscription, take } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
+const Modules: any[] = [MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatButtonModule,
+  FormsModule];
 @Component({
   selector: 'app-starships-list',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, ...Modules],
   templateUrl: './starships-list.component.html',
   styleUrl: './starships-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class StarshipsListComponent {
+export class StarshipsListComponent implements OnInit, OnDestroy {
   starships$ = this.store.select(selectAllStarships);
   selectedStarship$ = this.store.select(selectSelectedStarship);
   loading$ = this.store.select(selectLoading);
 
-  constructor(private store: Store<AppState>) {}
+  displayedColumns: string[] = ['name', 'manufacturer','starship_class','details'];
+  filteredStarships: any[] = [];
+  manufacturers: string[] = [];
+  searchText = '';
+  selectedManufacturer = '';
+  subscription: Subscription = new Subscription();
+
+  constructor(private store: Store<AppState>) { }
+
+
+
 
   ngOnInit() {
     this.store.dispatch(loadStarships());
+    this.fillManufacturersData();
   }
 
-  fetchStarshipDetails(url: string) {
-    const id = url.split('/').filter(Boolean).pop(); // Extract the ID from the URL
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  private fillManufacturersData() {
+    this.subscription.add(this.starships$.subscribe((starships) => {
+      if (starships) {
+        this.filteredStarships = starships;
+        this.manufacturers = Array.from(
+          new Set(starships.map((s) => s.manufacturer).filter((m) => !!m))
+        );
+      }
+    }));
+  }
+
+  filterStarships() {
+    this.starships$.subscribe((starships) => {
+      this.filteredStarships = starships.filter((starship) => {
+        const matchesSearch = starship.name
+          .toLowerCase()
+          .includes(this.searchText.toLowerCase());
+        const matchesManufacturer =
+          !this.selectedManufacturer || starship.manufacturer === this.selectedManufacturer;
+        return matchesSearch && matchesManufacturer;
+      });
+    });
+  }
+
+
+
+
+  viewDetails(url: string) {
+    const id = url.split('/').filter(Boolean).pop();
     if (id) this.store.dispatch(loadStarshipDetails({ id }));
   }
 }
