deb492f4baec8ca1d978d83ffa81974373d82cd1
AliZiabary11 <aliziabary@gmail.com>
2024-12-04 18:58:07 +0330

    add Starship List and StarshipsDetail Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { StarshipsListComponent } from './pages/starships-list/starships-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'main'
+    },
+    {
+        path: 'main',
+        component: StarshipsListComponent
+    },
+];
